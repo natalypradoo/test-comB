1aaa51b9bebd58a86a27e8de311f468a1e244d4f
Nataly Prado <113069046+natalypradoo@users.noreply.github.com>
2022-09-25 10:56:37 -0300

    git add .

--- file/práctica.js
+++ file/práctica.js
@@ -1,2 +1,8 @@
 //@autor: natalypradoo
 // este archivo solo es para practicar como empezar a usar Git en la Web
+ function getPersona (nombre); {
+    console.log(nombre);
+ }
+  git add .
+  git push u origin main 
+  
